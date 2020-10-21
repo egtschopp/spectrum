4357550e2f60c0709516ded5a7fd39d46bab96d1
egtschopp <72047225+egtschopp@users.noreply.github.com>
2020-10-21 10:03:49 -0600

    Add files via upload

---
+++ src/test/java/us/etschopp/spectrum/SpectrumApplicationTests.java
@@ -0,0 +1,13 @@
+package us.etschopp.spectrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpectrumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
